567849e0bf65567189564d6546649204c5a83c19
Nicclassy <lucanapoli122@gmail.com>
2026-01-02 21:42:31 +1100

    Add basic chance card functionality

--- MonopolySimulator/Chance.fs
+++ MonopolySimulator/Chance.fs
@@ -1,1 +1,27 @@
-namespace MonopolySimulator.Types
+namespace MonopolySimulator.Mechanics
+
+open MonopolySimulator.Types
+
+type ChanceCard = {
+    Description: string
+    Action: Board -> PlayerState -> PlayerState
+}
+
+module ChanceCards =
+    let moveTo (spaceId : SpaceId) =
+        let action (board : Board) (playerState : PlayerState) =
+            let space = board.[spaceId]
+            let position = board.PositionOf space
+            { playerState with MovementState = NextPosition position }
+        action
+
+    let deck: ChanceCard list = [
+        {
+            Description = "Advance to Go (Collect $200)"
+            Action = moveTo SpaceId.Go
+        }
+    ]
+
+type ChanceCards(cards: ChanceCard list) =
+    let cards = cards
+    new () = ChanceCards ChanceCards.deck 
