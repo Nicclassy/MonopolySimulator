759599de6645d459cf8864348a1fd5b0f4a30cef
Nicclassy <lucanapoli122@gmail.com>
2026-01-02 21:40:44 +1100

    Implement in jail turns

--- MonopolySimulator/Game.fs
+++ MonopolySimulator/Game.fs
@@ -3,50 +3,64 @@ namespace MonopolySimulator.Mechanics
 open MonopolySimulator.Types
 
 type GameState(players: Player array) =
-    let mutable players = players
-    let mutable playerStatesByCharacter = 
+    let players = players
+    let playerStatesByCharacter = 
         players
-        |> Array.map (fun player -> player.Character, PlayerState.Initial)
+        |> Array.map (fun player -> player.Character, PlayerState.Initial player.Character)
         |> Map.ofArray
 
     member _.Item
         with get(index: Character) = playerStatesByCharacter.[index]
 
 module GameMechanics =
-    let nextPlayerPosition state (board : Board) { DiceOne = diceOne; DiceTwo = diceTwo } = 
-        (state.Position + diceOne + diceTwo) % board.Length
+    let movePlayer (board : Board) { DiceOne = diceOne; DiceTwo = diceTwo } state = 
+        let rawPosition = state.Position + diceOne + diceTwo
+        let passedGo = rawPosition >= board.Length
+        let position = rawPosition % board.Length
+        { state with Position = position; PassedGo = passedGo }
 
-    let playerAction playerState board = ()
+    let playerAction (board : Board) (playerState : PlayerState) = 
+        let space = board.[playerState.Position]
+        playerState
 
-    let jailedPlayerTurn playerState board = 
-        let dice = Dice()
-        let { RollAgain = escapedJail} as roll = dice.Roll()
-        match playerState.JailedState with
+    let jailedPlayerTurn ({ State = playerState } as jailedPlayerState : JailedPlayerState) board = 
+        let escapeJail roll = 
+            playerState 
+                |> movePlayer board roll 
+                |> playerAction board
+
+        let { RollAgain = escapedJail} as roll = Dice.Roll()
+        match jailedPlayerState.TurnsJailed with
         | _ when escapedJail ->
-            // Do all in same place???~
-            playerState.Position <- nextPlayerPosition playerState board roll
-            playerAction playerState board
-        | Jailed TwoTurns ->
-            playerState.Position <- nextPlayerPosition playerState board roll
-            playerAction playerState board
-        | Jailed turns -> 
-            playerState.JailedState <- Jailed turns.Advance
-        | _ -> ()
+            escapeJail roll |> NotInJail
+        | TwoTurns ->
+            escapeJail roll |> NotInJail
+        | turns -> 
+            InJail { jailedPlayerState with TurnsJailed = turns.Advance }
 
-    let playerTurn playerState board = 
-        let mutable turnEnded = false
+    let playerTurn board playerState = 
         let dice = Dice()
-        while not turnEnded do
-            let roll = dice.Roll()
-            playerState.Position <- nextPlayerPosition playerState board roll 
-            playerAction playerState board
-            turnEnded <- not roll.RollAgain
-            if roll.JailedState = JailedState.Initial then
-                playerState.JailedState <- JailedState.Initial
-                turnEnded <- true
+        let rec turn () = 
+            match dice.Roll() with 
+            | Jailed -> 
+                InJail { 
+                    State = playerState
+                    TurnsJailed = NoTurns
+                }
+            | Roll roll ->
+                let playerState = 
+                    playerState 
+                    |> movePlayer board roll 
+                    |> playerAction board
 
-    let PlayerTurn playerState board = 
-        match playerState.JailedState with
-        | Jailed _ -> jailedPlayerTurn playerState board
-        | NotJailed -> playerTurn playerState board
+                if roll.RollAgain then
+                    NotInJail playerState
+                else
+                    turn ()   
+        turn ()
+                
+    let PlayerTurn (board : Board) playerTurnState = 
+        match playerTurnState with
+        | InJail jailedPlayerState -> jailedPlayerTurn jailedPlayerState board
+        | NotInJail playerState -> playerTurn board playerState
 
